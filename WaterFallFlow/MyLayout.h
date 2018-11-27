b1355e0ae69e16987b30a5a80d4cf72e7a6fc3a6
longhongwei <longhongwei@qiushibaike.com>
2018-11-27 15:00:59 +0800

    添加卡片流水式布局

---
+++ WaterFallFlow/MyLayout.h
@@ -0,0 +1,17 @@
+//
+//  MyLayout.h
+//  iOS
+//
+//  Created by hongweilong on 2018/11/27.
+//  Copyright © 2018 hongweilong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+// 添加一种卡片式布局
+@interface MyLayout : UICollectionViewFlowLayout
+
+@end
+
+NS_ASSUME_NONNULL_END
